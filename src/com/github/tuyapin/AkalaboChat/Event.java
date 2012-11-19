086cd17b5b827907c0e0becd0a37fc4c50347b3b
tuyapin <tuyapin0130@gmail.com>
2012-11-20 02:10:48 +0900

    Bukkit 1.4.2-R0.2

--- src/com/github/tuyapin/AkalaboChat/Event.java
+++ src/com/github/tuyapin/AkalaboChat/Event.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.github.tuyapin.AkalaboChat;
 
 import org.bukkit.ChatColor;
@@ -50,3 +51,34 @@ public final class Event
     }
 >>>>>>> Bukkit 1.3.2-R0.2 #2364
 }
+=======
+package com.github.tuyapin.AkalaboChat;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+
+@SuppressWarnings("deprecation")
+public class Event implements Listener {
+    
+    @EventHandler(priority=EventPriority.LOWEST)
+    public void onPlayerChat(PlayerChatEvent e)
+    {
+        //コマンド無視
+        if(e.getMessage().startsWith("/"))
+        {
+            return;
+        }
+        //2Byte文字を含む場合も除去(NihongoMOD,MinecraftIM対策)
+        if(e.getMessage().getBytes().length != e.getMessage().length())
+        {
+            return;
+        }
+        //
+        AkalaboChat.plugin.getServer().broadcastMessage(ChatColor.GOLD + "[ALC]" + Converter.convert(e.getMessage()));
+    }
+
+}
+>>>>>>> Bukkit 1.4.2-R0.2
